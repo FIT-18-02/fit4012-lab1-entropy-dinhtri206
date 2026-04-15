d3dd411022af289a322c861c0d2b32268b6028d7
Dtri06 <tridead1997@gmail.com>
2026-04-15 14:57:26 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
-
 using namespace std;
-
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
 
 int extended_gcd(int a, int b, int &x, int &y) {
     if (b == 0) {
-        x = 1; y = 0;
+        x = 1;
+        y = 0;
         return a;
     }
     int x1, y1;
@@ -27,24 +18,9 @@ int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_gcd(a, m, x, y);
 
-    if (g != 1) return -1; // QUAN TRỌNG
+    if (g != 1) return -1;  // không tồn tại nghịch đảo
 
     return (x % m + m) % m;
 }
 
-int main() {
-    int a = 0, m = 0;
-    cout << "Nhap a, m: ";
-    cin >> a >> m;
 
-    if (gcd(a, m) != 1) {
-        cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1.\n";
-        return 0;
-    }
-
-    int inv = mod_inverse(a, m);
-    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
-    cout << "Kiem tra: " << a << " * " << inv << " % " << m
-         << " = " << (1LL * a * inv % m) << '\n';
-    return 0;
-}
